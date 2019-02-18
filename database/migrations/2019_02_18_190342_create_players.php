59c6b499566a9207a9d2bea295b5331f60308a0d
Marina <marinaandjelkovic456@gmail.com>
2019-02-18 20:21:11 +0100

    zadatak 1

---
+++ database/migrations/2019_02_18_190342_create_players.php
@@ -0,0 +1,39 @@
+<?php
+
+use Illuminate\Support\Facades\Schema;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class CreatePlayers extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('players', function (Blueprint $table) {
+            $table->increments('id');
+            $table->string('first_name');
+            $table->string('last_name');
+            $table->string('email')->unique();
+            $table->unsignedInteger('team_id')->nullable();
+            $table->foreign('team_id')->references('id')->on('teams')->onDelete('cascade');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('players', function (Blueprint $table) {
+            $table->dropForeign('players_team_id_foreign');
+            $table->dropColumn('team_id');
+        });
+    }
+}
